7164555f0e826d20b5b23a2db53f56a479b24892
Owais Saleem <osaleem303@hotmail.com>
2020-11-19 12:48:12 +0500

    Binding Tasks Functionality with Backend

--- WebClient/Abstractions/ITaskDataService.cs
+++ WebClient/Abstractions/ITaskDataService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using WebClient.Shared.Models;
@@ -13,14 +14,14 @@ namespace WebClient.Abstractions
     /// </summary>
     public interface ITaskDataService
     {
-        List<TaskModel> Tasks { get; }
-        TaskModel SelectedTask { get; }
+        IEnumerable<TaskVm> Tasks { get; }
+        TaskVm SelectedTask { get; }
 
         event EventHandler TasksUpdated;
         event EventHandler TaskSelected;
 
         void SelectTask(Guid id);
-        void ToggleTask(Guid id);
-        void AddTask(TaskModel model);
+        Task ToggleTask(Guid id);
+        Task AddTask(TaskVm model);
     }
 }
